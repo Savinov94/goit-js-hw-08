1ace6bd33088aa2683cbe075c2f80ffffa77ed63
Savinov94 <134193164+Savinov94@users.noreply.github.com>
2023-10-06 16:29:14 +0200

    34985

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(async (data) => {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime.toString());
+}, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(parseFloat(savedTime));
+}
+
+player.ready().then(function () {
+}).catch(function (error) {
+  console.error('Помилка ініціалізації плеєра:', error);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+const updateLocalStorage = throttle(() => {
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}, 500);
+ 
+const fillFormFromLocalStorage = () => {
+  const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email || '';
+    messageTextarea.value = formData.message || '';
+  }
+};
+
+
+emailInput.addEventListener('input', updateLocalStorage);
+messageTextarea.addEventListener('input', updateLocalStorage);
+
+
+window.addEventListener('load', fillFormFromLocalStorage);
+
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  
+
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  console.log('Форма відправлена з такими даними:', formData);
+
+  emailInput.value = '';
+  messageTextarea.value = '';
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+});
